1160e60cbf067b6d0b08eb82d4cd6e047cee7228
Yan Hong <onlyhongyan@gmail.com>
2017-06-08 16:50:59 -0700

    show current remaining

--- pages/index/book.js
+++ pages/index/book.js
@@ -1,18 +1,21 @@
 // pages/index/book.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    bookInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      bookInfo: app.globalData.bookInfo,
+    })
   },
 
   /**
